a54ff3648988041ae521f1a329147775119a7e3d
Mikhail9030 <gulaevmihail112@gmail.com>
2021-01-08 22:15:33 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/manager/PostManager.java
@@ -0,0 +1,16 @@
+package ru.netology.domain.manager;
+
+import ru.netology.domain.manager;
+
+public class PostManager {
+    private PostManager[] posts;
+
+    public PostManager[] search(long ownerId, String domain, String query, boolean ownersOnly, int count, int offset){
+        return null;
+    }
+
+    public void delete(long ownerId, long postId){
+
+    }
+}
+
